28d1eb16c1e58bcd3d5d9c8ab6776e723062352d
merelbongenaar <70645161+merelbongenaar@users.noreply.github.com>
2021-05-30 09:17:29 +0200

    refresh timer

--- ChapeauOrderingSystem/ChapeauUI/TableOverview.cs
+++ ChapeauOrderingSystem/ChapeauUI/TableOverview.cs
@@ -26,8 +26,22 @@ namespace ChapeauUI
 
             btnAddItem.Hide();
             btnPayForOrder.Hide();
-            
+
+            RefreshTables();
+
+            System.Windows.Forms.Timer t = new System.Windows.Forms.Timer();
+            t.Interval = 15000; // specify interval time as you want
+            t.Tick += new EventHandler(timer_Tick);
+            t.Start();
+
         }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            RefreshTables();
+        }
+
+
 
         //----------------------------------------------------------------------------------TABLE BUTTONS-----------------------------------------------------------------------------------------------------------
         private void btnTable1_Click(object sender, EventArgs e)
