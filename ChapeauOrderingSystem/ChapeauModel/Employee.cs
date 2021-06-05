e2bc149bdab7add9b1a43e2ab486e0007a87fda2
merelbongenaar <70645161+merelbongenaar@users.noreply.github.com>
2021-06-05 18:43:00 +0200

    enum employee

--- ChapeauOrderingSystem/ChapeauModel/Employee.cs
+++ ChapeauOrderingSystem/ChapeauModel/Employee.cs
@@ -4,8 +4,10 @@ using System.Text;
 
 namespace ChapeauModel
 {
+    public enum Role { Bar=1, Kitchen, Waiter, Manager}
     public class Employee
     {
+
         public int EmployeeID { get; set; }
 
         public string Name { get; set; }
