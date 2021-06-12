767147dc2e53ad8a1a30a88a95e842e0049fb6f7
mm55355 <70644823+mm55355@users.noreply.github.com>
2021-06-12 23:08:32 +0200

    try catch tiptxtbox

--- ChapeauOrderingSystem/ChapeauUI/BillForm.cs
+++ ChapeauOrderingSystem/ChapeauUI/BillForm.cs
@@ -67,21 +67,28 @@ namespace ChapeauUI
         
         private void txtBoxTip_TextChanged(object sender, EventArgs e)
         {
-            // get the tip amount that the user will enter            
+            // get the tip amount that the user will enter     
             decimal tip = 0;
+            try
+            {
+                if (txtBoxTip.Text.Length > 0 && TxtBoxDigitsFilter(txtBoxTip.Text))
+                {
+                    tip = decimal.Parse(txtBoxTip.Text);
+                }
+                else if (!TxtBoxDigitsFilter(txtBoxTip.Text) && txtBoxTip.Text.Length > 0)
+                {
+                    MessageBox.Show("Invalid tip amount, please enter only digits!");
+                    txtBoxTip.Text = "";
+                }
+                else
+                {
+                    tip = 0;
+                }
+            }
+            catch (Exception t)
+            {
 
-            if (txtBoxTip.Text.Length > 0 && TxtBoxDigitsFilter(txtBoxTip.Text))
-            {
-                tip = decimal.Parse(txtBoxTip.Text);
-            }
-            else if (!TxtBoxDigitsFilter(txtBoxTip.Text) && txtBoxTip.Text.Length > 0)
-            {
-                MessageBox.Show("Invalid tip amount, please enter only digits!");
-                txtBoxTip.Text = "";
-            }
-            else
-            {  
-                tip = 0;                
+                MessageBox.Show("Something went wrong with the tip: " + t.Message);
             }
 
             // store the tip amount to the new bill object
@@ -95,6 +102,7 @@ namespace ChapeauUI
         // method to filter the txtbox to accept int only
         private bool TxtBoxDigitsFilter(string txt)
         {
+            
             foreach (char c in txt)
             {
                 if (c == '.')
